0dd8b093bc246761c946ead410fedec7bf8096b8
ji-e <33503786+ji-e@users.noreply.github.com>
2017-11-22 13:11:36 +0900

    2017.11.22

---
+++ common/js/LMMVIEWS/AddMeeting.js
@@ -0,0 +1,8 @@
+var Observable = require('FuseJS/Observable');
+function goback(){
+	router.goBack();
+}
+
+module.exports = {
+	goback:goback
+};
---
+++ common/js/LMMVIEWS/BackInProgressMeeting.js
@@ -0,0 +1,9 @@
+var Observable = require('FuseJS/Observable');
+
+function goToLoginPage(){
+	router.goto("LoginPage");
+
+}
+module.exports = {
+	goToLoginPage:goToLoginPage
+};
